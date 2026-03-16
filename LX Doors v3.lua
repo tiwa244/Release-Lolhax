ec12ce8e38077623fdd43643af0b10c9cab1ddc7
Lozero <159615172+tiwa244@users.noreply.github.com>
2026-03-16 16:55:22 +0700

    very cool change seusushwh

--- LX Doors v3.lua
+++ LX Doors v3.lua
@@ -2079,6 +2079,7 @@ function Esp(Parent, TextAdornee, Text, Color, OutlineColor, TextLabelColor)
         while Parent and not Library.Unloaded and task.wait() do
 			TextLabel.Visible = Toggles.ESPI_M_Enabled.Value
 			Highlight.Enabled = Toggles.ESPI_M_Enabled.Value
+			TextLabel.Font = Enum.Font[Options.ESPS_Font.Value]
 			TextLabel.TextSize = Options.ESPS_FontSize.Value
 		   if not Toggles.ESPI_M_Name.Value then
 			   Text = ""
@@ -2169,6 +2170,7 @@ function EspEntity(Parent, TextAdornee, Text, Color, OutlineColor, TextLabelColo
         while Parent and not Library.Unloaded and task.wait() do
 			TextLabel.Visible = Toggles.ESPE_Enabled.Value
 			Highlight.Enabled = Toggles.ESPE_Enabled.Value
+			TextLabel.Font = Enum.Font[Options.ESPS_Font.Value]
 			TextLabel.TextSize = Options.ESPS_FontSize.Value
 		   if not Toggles.ESPE_Name.Value then
 			   Text = ""
