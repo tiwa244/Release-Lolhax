4ae427a71da70664aafef9a4685b3e2161652df7
Lozero <159615172+tiwa244@users.noreply.github.com>
2026-03-17 11:30:40 +0700

    Update LX Doors v3.lua

--- LX Doors v3.lua
+++ LX Doors v3.lua
@@ -2076,7 +2076,7 @@ function Esp(Parent, TextAdornee, Text, Color, OutlineColor, TextLabelColor, Var
 	TextLabel:SetAttribute("SafeText", Text)
 
     task.spawn(function()
-        while Parent and not Library.Unloaded and task.wait() and LHXLoadFinish do
+        while Parent and not Library.Unloaded and task.wait() do
 			TextLabel.Visible = Toggles.ESPI_M_Enabled.Value and Toggles[prefix .. VarName].Value
 			Highlight.Enabled = Toggles.ESPI_M_Enabled.Value and Toggles[prefix .. VarName].Value
 			Highlight.FillColor = Options[prefix .. VarName .. "_F"].Value
