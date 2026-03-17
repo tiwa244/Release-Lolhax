67f186f4eb49ccc189c51dba8e81d5929bc12fa4
Lozero <159615172+tiwa244@users.noreply.github.com>
2026-03-17 09:19:19 +0700

    Update LX Doors v3.lua

--- LX Doors v3.lua
+++ LX Doors v3.lua
@@ -2230,7 +2230,124 @@ game:GetService("TweenService"):Create(
 
     return Highlight, TextLabel
 end
-	
+
+function EspGold(Parent, TextAdornee, Text, Color, OutlineColor, TextLabelColor, VarName)
+
+    local BillboardGui = Instance.new("BillboardGui", Parent)
+    local TextLabel = Instance.new("TextLabel", BillboardGui)
+    local Highlight = Instance.new("Highlight", Parent)
+
+    BillboardGui.Adornee = TextAdornee
+    BillboardGui.AlwaysOnTop = true
+    BillboardGui.Name = "_LOLHAXBG"
+    BillboardGui.Size = UDim2.fromScale(1,1)
+    BillboardGui.Enabled = true
+
+    Highlight.Name = "_LOLHAXHL"
+    Highlight.Adornee = Parent
+
+    TextLabel.Size = UDim2.fromScale(1,1)
+    TextLabel.TextStrokeTransparency = 0
+    TextLabel.Font = Enum.Font[Options.ESPS_Font.Value]
+    TextLabel.TextSize = Options.ESPS_FontSize.Value
+    TextLabel.TextColor3 = TextLabelColor or Color
+    TextLabel.BackgroundTransparency = 1
+
+    Highlight.FillColor = Color
+    Highlight.OutlineColor = OutlineColor or Color
+
+    TextLabel.TextTransparency = 1
+    Highlight.FillTransparency = 1
+    Highlight.OutlineTransparency = 1
+
+    TextLabel:SetAttribute("Text", Text)
+    TextLabel:SetAttribute("SafeText", Text)
+
+    task.spawn(function()
+        while Parent and not Library.Unloaded and task.wait() do
+
+            local Toggle = Toggles.ESPI_C_GoldPiles
+
+            TextLabel.Visible =
+                Toggles.ESPI_M_Enabled.Value
+                and Toggle
+                and Toggle.Value
+
+            Highlight.Enabled =
+                Toggles.ESPI_M_Enabled.Value
+                and Toggle
+                and Toggle.Value
+
+            Highlight.FillColor = Color
+            Highlight.OutlineColor = OutlineColor
+            TextLabel.TextColor3 = TextLabelColor or Color
+
+            TextLabel.Font = Enum.Font[Options.ESPS_Font.Value]
+            TextLabel.TextSize = Options.ESPS_FontSize.Value
+
+            if not Toggles.ESPI_M_Name.Value then
+                Text = ""
+            else
+                Text = TextLabel:GetAttribute("SafeText")
+            end
+
+            if not Toggles.ESPI_M_Fill.Value then
+                Highlight.FillTransparency = 1
+            else
+                Highlight.FillTransparency = Options.ESPS_FillTransparency.Value
+            end
+
+            if not Toggles.ESPI_M_Outline.Value then
+                Highlight.OutlineTransparency = 1
+            else
+                Highlight.OutlineTransparency = Options.ESPS_OutlineTransparency.Value
+            end
+
+            local Distance =
+                (workspace.CurrentCamera.CFrame.Position - Parent:GetPivot().Position).Magnitude
+
+            if Toggles.ESPI_M_Distance.Value then
+                TextLabel.Text =
+                    Text .. "\n[ " ..
+                    string.format(Distance <= 9.9 and "%.1f" or "%.0f", Distance)
+                    .. " ]"
+            else
+                TextLabel.Text = Text
+            end
+        end
+    end)
+
+    game:GetService("TweenService"):Create(
+        Highlight,
+        TweenInfo.new(Options.ESPS_FadeTime.Value),
+        {
+            FillTransparency =
+                Toggles.ESPI_M_Fill.Value
+                and Options.ESPS_FillTransparency.Value
+                or 1
+        }
+    ):Play()
+
+    game:GetService("TweenService"):Create(
+        Highlight,
+        TweenInfo.new(Options.ESPS_FadeTime.Value),
+        {
+            OutlineTransparency =
+                Toggles.ESPI_M_Outline.Value
+                and Options.ESPS_OutlineTransparency.Value
+                or 1
+        }
+    ):Play()
+
+    game:GetService("TweenService"):Create(
+        TextLabel,
+        TweenInfo.new(Options.ESPS_FadeTime.Value),
+        {TextTransparency = 0}
+    ):Play()
+
+    return Highlight, TextLabel
+end
+
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
@@ -3481,7 +3598,7 @@ local Connections = {
             elseif v.Name == "GoldPile" then
                 v:WaitForChild("Hitbox", 9e9)
 
-                local Highlight, TextLabel = Esp(v, v, "Gold Pile [ "..v:GetAttribute("GoldValue").." ]", Options.ESPI_C_GoldPiles_F.Value, Options.ESPI_C_GoldPiles_O.Value)
+                local Highlight, TextLabel = EspGold(v, v, "Gold Pile [ "..v:GetAttribute("GoldValue").." ]", Options.ESPI_C_GoldPiles_F.Value, Options.ESPI_C_GoldPiles_O.Value)
                 table.insert(EspTable.Interactables.GoldPiles, {Highlight, TextLabel})
  
             elseif v.Name == "LeverForGate" then
@@ -5533,7 +5650,7 @@ for _, v in Rooms:GetDescendants() do
 
             elseif v.Name == "GoldPile" then
                 
-                local Highlight, TextLabel = Esp(v, v, "Gold Pile [ "..v:GetAttribute("GoldValue").." ]", Options.ESPI_C_GoldPiles_F.Value, Options.ESPI_C_GoldPiles_O.Value)
+                local Highlight, TextLabel = EspGold(v, v, "Gold Pile [ "..v:GetAttribute("GoldValue").." ]", Options.ESPI_C_GoldPiles_F.Value, Options.ESPI_C_GoldPiles_O.Value)
                 table.insert(EspTable.Interactables.GoldPiles, {Highlight, TextLabel})
 
             elseif v.Name == "LeverForGate" and not v.ActivateEventPrompt:GetAttribute("Interactions") then
