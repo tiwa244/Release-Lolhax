5095f096708d03d3a24c43d1132bb7dea5517327
Lozero <159615172+tiwa244@users.noreply.github.com>
2026-03-17 10:38:43 +0700

    Update LX Doors v3.lua

--- LX Doors v3.lua
+++ LX Doors v3.lua
@@ -2041,9 +2041,7 @@ end
 function Esp(Parent, TextAdornee, Text, Color, OutlineColor, TextLabelColor, VarName)
     -- rmved
 	-- new new
-	if not VarName then 
-		warn("missing varname for " .. Text)
-	end
+	print(VarName)
     local BillboardGui = Instance.new("BillboardGui", Parent)
     local TextLabel = Instance.new("TextLabel", BillboardGui)
     local Highlight = Instance.new("Highlight", Parent)
