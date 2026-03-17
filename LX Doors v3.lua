787ba025d791864ea44f6359fca552679b9bc13b
Lozero <159615172+tiwa244@users.noreply.github.com>
2026-03-17 15:34:02 +0700

    Update LX Doors v3.lua

--- LX Doors v3.lua
+++ LX Doors v3.lua
@@ -1129,7 +1129,7 @@ ESPInteractables_Configurate:AddDropdown("ESPI_C_Style", {
 })
 
 local CustomColors = {}
-{
+local Items = {
     {Tag="Doors", Text="Door", Color=Color3.fromRGB(0,255,127), Color2=Color3.new(1,1,1), Color4=Color3.new(1,1,1)},
     {Tag="DoorKeys", Text="Door Key", Color=Color3.fromRGB(255,174,0), Color2=Color3.new(1,1,1), Color4=Color3.new(1,1,1)},
     {Tag="GoldPiles", Text="Gold Piles", Color=Color3.new(1,1,1), NoText=false, Color2=Color3.new(1,1,1), Color4=Color3.new(1,1,1)},
