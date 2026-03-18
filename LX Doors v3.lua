7ade36f09fcae45c68aad6246a5cf86b29845c48
Lozero <159615172+tiwa244@users.noreply.github.com>
2026-03-18 13:03:47 +0700

    Update LX Doors v3.lua

--- LX Doors v3.lua
+++ LX Doors v3.lua
@@ -487,7 +487,7 @@ do
         UICorner.CornerRadius = UDim.new(0, 2)
 
         local UIStroke = Instance.new("UIStroke", Frame)
-        UIStroke.Thickness = 2
+        UIStroke.Thickness = 1
 	
 	    Library:AddToRegistry(UIStroke, {
         Color = "AccentColor"
