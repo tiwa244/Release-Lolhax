cb02828b382143badda7812bc888870d5ae35bf2
Lozero <159615172+tiwa244@users.noreply.github.com>
2026-03-20 23:29:03 +0700

    Update LX Doors v3.lua

--- LX Doors v3.lua
+++ LX Doors v3.lua
@@ -3688,9 +3688,8 @@ local Connections = {
 
                 v:WaitForChild("Hitbox", 9e9)
 
-				local KeyID = v:GetAttribute("KeyID")
-
                 task.delay(1, function()
+					local KeyID = v:GetAttribute("KeyID")
                     local Highlight, TextLabel = Esp(v, v, "( " .. tostring(KeyID) .. " )" .. "Door Key", Options.ESPI_C_DoorKeys_F.Value, Options.ESPI_C_DoorKeys_O.Value, Options.ESPI_C_DoorKeys_TC.Value, "DoorKeys", "Interactable")
                     table.insert(EspTable.Interactables.DoorKeys, {Highlight, TextLabel})
                 end)
@@ -5732,7 +5731,11 @@ for _, v in Rooms:GetDescendants() do
                 
             elseif v.Name == "KeyObtain" then
 
-				local KeyID = v:GetAttribute("KeyID")
+		    local KeyID
+
+            v:GetAttributeChangedSignal("KeyID"):Connect(function()
+                KeyID = v:GetAttribute("KeyID")
+            end)
 
                 local Highlight, TextLabel = Esp(v, v, "( " .. tostring(KeyID) .. " )" .. "Door Key", Options.ESPI_C_DoorKeys_F.Value, Options.ESPI_C_DoorKeys_O.Value, Options.ESPI_C_DoorKeys_TC.Value, "DoorKeys", "Interactable")
                 table.insert(EspTable.Interactables.DoorKeys, {Highlight, TextLabel})
