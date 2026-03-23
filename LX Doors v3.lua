04d542faa98ccd39d225fde1c0e141c935f732ca
Lozero <159615172+tiwa244@users.noreply.github.com>
2026-03-23 19:02:26 +0700

    Update LX Doors v3.lua

--- LX Doors v3.lua
+++ LX Doors v3.lua
@@ -167,170 +167,7 @@ end
 local Script = shared.Script
 Script.Functions = {}
 shared.Humanoid = game.Players.LocalPlayer.Character.Humanoid
-Script.Functions.EnforceTypes = function(args, template)
-    args = if typeof(args) == "table" then args else {}
 
-    for key, value in pairs(template) do
-        local argValue = args[key]
-
-        if argValue == nil or (value ~= nil and typeof(argValue) ~= typeof(value)) then
-            args[key] = value
-        elseif typeof(value) == "table" then
-            args[key] = Script.Functions.EnforceTypes(argValue, value)
-        end
-    end
-
-    return args
-end
-
-function ForceCheckboxSwitch(Value)
-    -- its not loaded so refhrn snd
-    if not LHXLoadFinish then 
-        print("ignored:", Value)
-        return 
-    end
-
-    if ChangeForceValue ~= config.ForceCheckbox then
-        config.ForceCheckbox = Value
-		--Library.ForceCheckbox = Value
-        writefile(filename, HttpService:JSONEncode(config))
-        print("saved ye " .. tostring(Value))
-    end
-end
-	
-local Doors = {}
-local mainUI
-
-function Doors:Notify(unsafeOptions)
-    assert(typeof(unsafeOptions) == "table", "Expected a table as options argument but got " .. typeof(unsafeOptions))
-
-    mainUI = mainUI or game.Players.LocalPlayer.PlayerGui:WaitForChild("GlobalUI", 2.5)
-    if not mainUI then return end
-
-    local options = Script.Functions.EnforceTypes(unsafeOptions, {
-        Title = "Notification",
-        Description = "No Text",
-        Reason = "",
-        NotificationType = "NOTIFICATION",
-        Image = "6023426923",
-        Color = nil,
-        Time = nil,
-
-        TweenDuration = 0.8
-    })
-
-    local acheivement = mainUI.AchievementsHolder.Achievement:Clone()
-    acheivement.Size = UDim2.new(0, 0, 0, 0)
-    acheivement.Frame.Position = UDim2.new(1.1, 0, 0, 0)
-    acheivement.Name = "LiveAchievement"
-    acheivement.Visible = true
-
-    acheivement.Frame.TextLabel.Text = options.NotificationType
-
-    if options.Color ~= nil then
-        acheivement.Frame.TextLabel.TextColor3 = options.Color
-        acheivement.Frame.UIStroke.Color = options.Color
-        acheivement.Frame.Glow.ImageColor3 = options.Color
-    end
-    
-    acheivement.Frame.Details.Desc.Text = tostring(options.Description)
-    acheivement.Frame.Details.Title.Text = tostring(options.Title)
-    acheivement.Frame.Details.Reason.Text = tostring(options.Reason or "")
-
-    if options.Image:match("rbxthumb://") or options.Image:match("rbxassetid://") then
-        acheivement.Frame.ImageLabel.Image = tostring(options.Image or "rbxassetid://0")
-    else
-        acheivement.Frame.ImageLabel.Image = "rbxassetid://" .. tostring(options.Image or "0")
-    end
-
-    acheivement.Parent = mainUI.AchievementsHolder
-    acheivement.Sound.SoundId = "rbxassetid://10469938989"
-
-    acheivement.Sound.Volume = 1
-
-        acheivement.Sound:Play()
-
-    task.spawn(function()
-        acheivement:TweenSize(UDim2.new(1, 0, 0.2, 0), "In", "Quad", options.TweenDuration, true)
-    
-        task.wait(0.8)
-    
-        acheivement.Frame:TweenPosition(UDim2.new(0, 0, 0, 0), "Out", "Quad", 0.5, true)
-    
-        game.TweenService:Create(acheivement.Frame.Glow, TweenInfo.new(1, Enum.EasingStyle.Quad, Enum.EasingDirection.In),{
-            ImageTransparency = 1
-        }):Play()
-    
-        if options.Time ~= nil then
-            if typeof(options.Time) == "number" then
-                task.wait(options.Time)
-            elseif typeof(options.Time) == "Instance" then
-                options.Time.Destroying:Wait()
-            end
-        else
-            task.wait(5)
-        end
-    
-        acheivement.Frame:TweenPosition(UDim2.new(1.1, 0, 0, 0), "In", "Quad", 0.5, true)
-        task.wait(0.5)
-        acheivement:TweenSize(UDim2.new(1, 0, -0.1, 0), "InOut", "Quad", 0.5, true)
-        task.wait(0.5)
-        acheivement:Destroy()
-    end)
-end
-
-function Doors:Alert(options)
-    assert(typeof(options) == "table", "Expected a table as options argument but got " .. typeof(options))
-
-    options["NotificationType"] = "WARNING"
-    options["Color"] = Color3.new(1, 0, 0)
-    options["TweenDuration"] = 0.3
-
-    Doors:Notify(options)
-end
-
-function Doors:Warn(options) Doors:Alert(options) end
-
-task.spawn(function()
-    -- 
-    repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer
-    
-    local player = game.Players.LocalPlayer
-
-    while task.wait() do
-        if Library.Unloaded then break end
-        
-        -- yes
-        if player and shared.Script then
-            pcall(function()
-                local attr = player:GetAttribute("CurrentRoom") or 0
-                shared.Script.CurrentRoom = attr
-
-                -- neinmare
-                if not workspace.CurrentRooms:FindFirstChild(tostring(attr)) then
-                    local latestObj = game:GetService("ReplicatedStorage"):FindFirstChild("GameData") 
-                                      and game.ReplicatedStorage.GameData:FindFirstChild("LatestRoom")
-                    
-                    if latestObj then
-                        shared.Script.CurrentRoom = latestObj.Value
-                        player:SetAttribute("CurrentRoom", latestObj.Value)
-                    end
-                end
-            end)
-        end
-    end
-end)
-
-function Script.Functions.CalculateHideTime(room: number)
-    for _, range in ipairs(Script.HideTimeValues) do
-        if room >= range.min and room <= range.max then
-            return math.round(range.a * (room - range.b) + range.c)
-        end
-    end    
-
-    return nil
-end
-	
 Script.HideTimeValues = {
     {min = 1, max = 5, a = -1/6, b = 1, c = 20},
     {min = 6, max = 19, a = -1/13, b = 6, c = 19},
@@ -483,6 +320,8 @@ do
     end
 end
 
+local Doors = {}
+local mainUI
 local HttpService = game:GetService("HttpService")
 local foldername = "lolhax"
 local filename = foldername .. "/LibraryConfig.json"
@@ -1632,7 +1471,168 @@ function SwitchNotify(notifyName)
         print("nah, config overwrite denied")
     end
 end
+
+Script.Functions.EnforceTypes = function(args, template)
+    args = if typeof(args) == "table" then args else {}
+
+    for key, value in pairs(template) do
+        local argValue = args[key]
+
+        if argValue == nil or (value ~= nil and typeof(argValue) ~= typeof(value)) then
+            args[key] = value
+        elseif typeof(value) == "table" then
+            args[key] = Script.Functions.EnforceTypes(argValue, value)
+        end
+    end
+
+    return args
+end
+
+function ForceCheckboxSwitch(Value)
+    -- its not loaded so refhrn snd
+    if not LHXLoadFinish then 
+        print("ignored:", Value)
+        return 
+    end
+
+    if ChangeForceValue ~= config.ForceCheckbox then
+        config.ForceCheckbox = Value
+		--Library.ForceCheckbox = Value
+        writefile(filename, HttpService:JSONEncode(config))
+        print("saved ye " .. tostring(Value))
+    end
+end
 	
+function Doors:Notify(unsafeOptions)
+    assert(typeof(unsafeOptions) == "table", "Expected a table as options argument but got " .. typeof(unsafeOptions))
+
+    mainUI = mainUI or game.Players.LocalPlayer.PlayerGui:WaitForChild("GlobalUI", 2.5)
+    if not mainUI then return end
+
+    local options = Script.Functions.EnforceTypes(unsafeOptions, {
+        Title = "Notification",
+        Description = "No Text",
+        Reason = "",
+        NotificationType = "NOTIFICATION",
+        Image = "6023426923",
+        Color = nil,
+        Time = nil,
+
+        TweenDuration = 0.8
+    })
+
+    local acheivement = mainUI.AchievementsHolder.Achievement:Clone()
+    acheivement.Size = UDim2.new(0, 0, 0, 0)
+    acheivement.Frame.Position = UDim2.new(1.1, 0, 0, 0)
+    acheivement.Name = "LiveAchievement"
+    acheivement.Visible = true
+
+    acheivement.Frame.TextLabel.Text = options.NotificationType
+
+    if options.Color ~= nil then
+        acheivement.Frame.TextLabel.TextColor3 = options.Color
+        acheivement.Frame.UIStroke.Color = options.Color
+        acheivement.Frame.Glow.ImageColor3 = options.Color
+    end
+    
+    acheivement.Frame.Details.Desc.Text = tostring(options.Description)
+    acheivement.Frame.Details.Title.Text = tostring(options.Title)
+    acheivement.Frame.Details.Reason.Text = tostring(options.Reason or "")
+
+    if options.Image:match("rbxthumb://") or options.Image:match("rbxassetid://") then
+        acheivement.Frame.ImageLabel.Image = tostring(options.Image or "rbxassetid://0")
+    else
+        acheivement.Frame.ImageLabel.Image = "rbxassetid://" .. tostring(options.Image or "0")
+    end
+
+    acheivement.Parent = mainUI.AchievementsHolder
+    acheivement.Sound.SoundId = "rbxassetid://10469938989"
+
+    acheivement.Sound.Volume = 1
+
+        acheivement.Sound:Play()
+
+    task.spawn(function()
+        acheivement:TweenSize(UDim2.new(1, 0, 0.2, 0), "In", "Quad", options.TweenDuration, true)
+    
+        task.wait(0.8)
+    
+        acheivement.Frame:TweenPosition(UDim2.new(0, 0, 0, 0), "Out", "Quad", 0.5, true)
+    
+        game.TweenService:Create(acheivement.Frame.Glow, TweenInfo.new(1, Enum.EasingStyle.Quad, Enum.EasingDirection.In),{
+            ImageTransparency = 1
+        }):Play()
+    
+        if options.Time ~= nil then
+            if typeof(options.Time) == "number" then
+                task.wait(options.Time)
+            elseif typeof(options.Time) == "Instance" then
+                options.Time.Destroying:Wait()
+            end
+        else
+            task.wait(5)
+        end
+    
+        acheivement.Frame:TweenPosition(UDim2.new(1.1, 0, 0, 0), "In", "Quad", 0.5, true)
+        task.wait(0.5)
+        acheivement:TweenSize(UDim2.new(1, 0, -0.1, 0), "InOut", "Quad", 0.5, true)
+        task.wait(0.5)
+        acheivement:Destroy()
+    end)
+end
+
+function Doors:Alert(options)
+    assert(typeof(options) == "table", "Expected a table as options argument but got " .. typeof(options))
+
+    options["NotificationType"] = "WARNING"
+    options["Color"] = Color3.new(1, 0, 0)
+    options["TweenDuration"] = 0.3
+
+    Doors:Notify(options)
+end
+
+function Doors:Warn(options) Doors:Alert(options) end
+
+task.spawn(function()
+    -- 
+    repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer
+    
+    local player = game.Players.LocalPlayer
+
+    while task.wait() do
+        if Library.Unloaded then break end
+        
+        -- yes
+        if player and shared.Script then
+            pcall(function()
+                local attr = player:GetAttribute("CurrentRoom") or 0
+                shared.Script.CurrentRoom = attr
+
+                -- neinmare
+                if not workspace.CurrentRooms:FindFirstChild(tostring(attr)) then
+                    local latestObj = game:GetService("ReplicatedStorage"):FindFirstChild("GameData") 
+                                      and game.ReplicatedStorage.GameData:FindFirstChild("LatestRoom")
+                    
+                    if latestObj then
+                        shared.Script.CurrentRoom = latestObj.Value
+                        player:SetAttribute("CurrentRoom", latestObj.Value)
+                    end
+                end
+            end)
+        end
+    end
+end)
+
+function Script.Functions.CalculateHideTime(room: number)
+    for _, range in ipairs(Script.HideTimeValues) do
+        if room >= range.min and room <= range.max then
+            return math.round(range.a * (room - range.b) + range.c)
+        end
+    end    
+
+    return nil
+end
+
 -- this is modified version of the lolhaxv2 get player function!
 
 function HasItem(Item)
