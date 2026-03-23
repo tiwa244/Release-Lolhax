5ec0ebb2f94ef39fcaff2000c3c1f925f4f6a166
Lozero <159615172+tiwa244@users.noreply.github.com>
2026-03-23 20:30:47 +0700

    Update LX Doors v3.lua

--- LX Doors v3.lua
+++ LX Doors v3.lua
@@ -6588,7 +6588,7 @@ task.spawn(function()
     DebugStuff:AddToggle("DS_BSRPC", { Text = "Bloxstrap RPC", Default = true })
     DebugStuff:AddLabel("Floor: " .. game.ReplicatedStorage.GameData.Floor.Value)
     DebugStuff:AddLabel("lolhax version: 3.0.2.5b")
-	DebugStuff:AddLabel("lolhax commit: idk test test")									
+	DebugStuff:AddLabel("lolhax commit: test checkbox#2")									
 
     local RPCRoomChange = game.ReplicatedStorage.GameData.LatestRoom:GetPropertyChangedSignal("Value"):Connect(function() updateRPC(Toggles.DS_BSRPC.Value) end)
     table.insert(Connections, RPCRoomChange)
