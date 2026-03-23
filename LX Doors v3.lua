4651e8eac452c023e5cc14bba1c9e92f23eb549c
Lozero <159615172+tiwa244@users.noreply.github.com>
2026-03-23 12:26:45 +0700

    Update LX Doors v3.lua

--- LX Doors v3.lua
+++ LX Doors v3.lua
@@ -776,13 +776,13 @@ ESPInteractables_Main:AddToggle("ESPI_M_CustomTC", { Text = "Custom Name Color",
 ESPInteractables_Main:AddToggle("ESPI_M_CustomColor_Outline", { Text = "Custom Outline Color", Default = false })
 :AddColorPicker("ESPI_Color_TC", { Default = Color3.new(1, 1, 1), Title = "Custom NameTag Color" })
 :AddColorPicker("ESPI_Color_Outline", { Default = Color3.new(1, 1, 1), Title = "Custom Outline Color" })
+ESPInteractables_Main:AddDivider("Extra")
 ESPInteractables_Main:AddToggle("ESPI_M_Tracers", { Text = "Tracers", Default = false, Tooltip = "Tracers."})
 ESPInteractables_Main:AddDropdown("ESPI_V_TracerPos", {
     Text = "Tracer Position",
     Values = { "Top", "Mouse", "Center", "Bottom" },
     Default = "Bottom"
 })
-ESPInteractables_Main:AddDivider("Extra")
 ESPInteractables_Main:AddToggle("ESPI_M_Arrows", { Text = "Arrows", Default = false, Tooltip = "Arrows."})
 ESPInteractables_Main:AddSlider("ESPI_M_ArrowsOffSet", { Text = "Arrow Offset", Default = 300, Min = 100, Max = 500, Rounding = 1,   Tooltip = "Arrow Offset."})
 local RainbowToggle = ESPInteractables_Main:AddToggle("ESPI_RAINBOW_HIGHLIGHT", { 
