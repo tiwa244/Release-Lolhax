30ac805333ee54e4fd1482833c982fd2ab7b0117
Lozero <159615172+tiwa244@users.noreply.github.com>
2026-03-27 12:41:20 +0700

    Update LX Doors v3.lua

--- LX Doors v3.lua
+++ LX Doors v3.lua
@@ -2071,7 +2071,7 @@ function Library:Notify(options, description, duration, force)
 
     -- fallback
     return SafeCall(function()
-        return Notify(options, data.Time)
+        return Notify(data.Title, data.Description, data.Time, data.Force)
     end)
 end
 
