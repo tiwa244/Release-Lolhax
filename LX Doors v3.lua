257bcbbe556e1b34a085ae58da9e266abe5ef60f
Lozero <159615172+tiwa244@users.noreply.github.com>
2026-03-28 08:36:35 +0700

    Update LX Doors v3.lua

--- LX Doors v3.lua
+++ LX Doors v3.lua
@@ -1583,7 +1583,7 @@ function AllAnchorsActivated()
 end
 
 function GrumbleNearby(threshold)
-    threshold = threshold or 170
+    threshold = threshold or 190
     local Grumble = Rooms[50]._NestHandler:FindFirstChild("_QueenGrumbleNest")
     if not Grumble then return false end
 
@@ -3402,7 +3402,7 @@ local Connections = {
 		})
     end
 				
-	if Script.IsMines and Script.Bypassed and LocalPlayer:GetAttribute("CurrentRoom") == 51 and GrumbleNearby(170) and AllAnchorsActivated() then
+	if Script.IsMines and Script.Bypassed and LocalPlayer:GetAttribute("CurrentRoom") == 51 and GrumbleNearby(190) and AllAnchorsActivated() then
 		Script.Bypassed = false
 		local idk2 = Instance.new("Folder", shared.Script.Workspace or Workspace)
 		idk2.Name = "_internal_lhx_acbypassprogress"
