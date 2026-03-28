de68326f61b4651955ba7f2d0d6e8ef07c7345bd
Lozero <159615172+tiwa244@users.noreply.github.com>
2026-03-28 09:20:31 +0700

    Update LX Doors v3.lua

--- LX Doors v3.lua
+++ LX Doors v3.lua
@@ -6615,6 +6615,27 @@ task.spawn(function()
         end
     end,
     })
+	MenuProperties:AddDropdown("NotifySide", {
+		Text = "Notification Side",
+		Tooltip = "This wont apply to doors notification style or default btw.",
+	    Values = { "Right", "Left" },
+		Default = "Right",
+		Callback = function(value)
+		if Options.NotifySide == "Default" or Options.NotifySide == "Doors" then return false end
+		if Options.NotifySide == "Obsidian" then
+			Library:SetNotifySide(value)
+		elseif Options.NotifySide == "Linoria" then
+			Library:SetNotifySide(value)
+		end
+		if LHXLoadFinish then
+		   Library:Notify({
+			 Title = "[LOLHAX]",
+			 Description = "Notification Side Changed to: " .. tostring(value),
+			 Time = 5
+		})
+		end
+	end,
+	})
     MenuProperties:AddDivider("Config")
     MenuProperties:AddButton("LX Discord Server", function()
      setclipboard("https://discord.gg/3xqFjM4R")
